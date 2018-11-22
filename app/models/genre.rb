6a506b8f9ee1e3dafd5f5b6ef449298224473f1a
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-22 18:37:25 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,10 +15,8 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     artists_names = []
-    i = 0
-    for self.songs[i]
+    self.songs[i].each do |i, artist|
       artists_names << self.songs[i].artist.name
-      i+1
     end
     artists_names
   end
